f8afb044cd8ba15c3dc07d2ee9f05f9da190214d
Janith Ravishan <jravishan@gmail.com>
2024-12-03 21:41:08 +0530

    fix

--- src/pages/Home_2/Products/Products.jsx
+++ src/pages/Home_2/Products/Products.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SelectVariants from './SelectVariants'
 
 const Products = () => {
     return (
@@ -6,8 +7,10 @@ const Products = () => {
             <div className='flex justify-center'>
                 <div className='w-[1024px] bg-white p-0 m-0'>
                     <div className='flex'>
-                        <div className='w-2/6'>1</div>
-                        <div className='w-4/6'>2</div>
+                        <div className='w-2/6 border'>
+                        <SelectVariants></SelectVariants>
+                        </div>
+                        <div className='w-4/5 border'>2</div>
                     </div>
                 </div>
             </div>
