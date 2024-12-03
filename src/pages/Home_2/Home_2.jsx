b10ad8574f968a801f5ab863d9c90371d8e2f726
Janith Ravishan <jravishan@gmail.com>
2024-12-03 20:08:44 +0530

    fix

--- src/pages/Home_2/Home_2.jsx
+++ src/pages/Home_2/Home_2.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import ImageSlider from './ImageSlider';
 import CustomizedTimeline from './CustomizedTimeline';
+import ImageMasonry from './ImageMasonry';
+import { Container } from '@mui/material';
 
 const Home_2 = () => {
   return (
-    <div className='bg-gray-400'>
+    <div className='flex justify-center'>
+      <div className='w-[1024px] bg-white p-0 m-0'>
+        <div className='p-0 m-0'>
+          <div className="md:flex gap-4">
+            <div className="md:w-3/4 border">
+              <ImageSlider images={images} />
+            </div>
+            <div className="md:w-1/4 border">
+              <div className="h-1/2 border">1</div>
+              <div className="h-1/2 border">2</div>
+            </div>
+          </div>
 
-      <div className="grid grid-cols-3 grid-rows-11 gap-4">
-        <div className="col-span-2 row-span-2 border">
-          <ImageSlider images={images} />
+          <div className="mt-4 border">
+            <ImageMasonry></ImageMasonry>
+          </div>
+
+          <div className="mt-4 border">
+            <CustomizedTimeline></CustomizedTimeline>
+          </div>
         </div>
-        <div className="col-span-3 row-span-2 col-start-1 row-start-3 border">
-          <CustomizedTimeline></CustomizedTimeline>
-        </div>
-        <div className="col-start-3 row-start-1 border">3</div>
-        <div className="col-start-3 row-start-2 border">4</div>
-        <div className="col-span-2 row-span-3 col-start-2 row-start-5 border">5</div>
-        <div className="col-start-1 row-start-5 border">6</div>
-        <div className="row-start-6 border">7</div>
-        <div className="row-start-7 border">8</div>
-        <div className="col-span-3 row-span-2 row-start-8 border">9</div>
-        <div className="col-span-2 row-start-10 border">10</div>
-        <div className="col-start-3 row-start-10 border">11</div>
-        <div className="col-span-3 row-start-11 border">Footer</div>
+
       </div>
-
     </div>
   )
 }
